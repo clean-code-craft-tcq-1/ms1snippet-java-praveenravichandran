6b8c54c47abdd3f628ed1c011eac59d912db5680
Praveen <ravichandran.praveen@in.bosch.com>
2021-03-31 11:39:18 +0530

    review comments fixed

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -2,29 +2,34 @@ package sensorval;
 
 import java.util.List;
 
-public class SensorValidator 
-{
-    public static boolean checkIfDifferenceExceedsThreshold(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean validateSOCreadings(List<Double> values) {
-        return values != null ? validateDifferenceBetweenValues(values) : false;
-    }
-    
-    public static boolean validateCurrentreadings(List<Double> values) {
-    	return values != null ? validateDifferenceBetweenValues(values) : false;
-    }
-    
-    private static boolean validateDifferenceBetweenValues(List<Double> values) {
-    	int lastButOneIndex = values.size() - 1;
-    	for(int i = 0; i < lastButOneIndex; i++) {
-    		if(!checkIfDifferenceExceedsThreshold(values.get(i), values.get(i + 1), 0.05)) {
-    			return false;
-    		}
-    	}
-    	return true;
-    }
+public class SensorValidator {
+	public static boolean checkIfDifferenceNotExceedsThreshold(double value, double nextValue, double maxDelta) {
+		if (nextValue - value > maxDelta) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateSOCreadings(List<Double> values) {
+		return values != null && !values.isEmpty() ? validateDifferenceBetweenValues(values) : false;
+	}
+
+	public static boolean validateCurrentreadings(List<Double> values) {
+		return values != null && !values.isEmpty() ? validateDifferenceBetweenValues(values) : false;
+	}
+
+	private static boolean validateDifferenceBetweenValues(List<Double> values) {
+		boolean isDifferenceWithinLimit = true;
+		try {
+			int lastButOneIndex = values.size() - 1;
+			for (int i = 0; i < lastButOneIndex; i++) {
+				if (!checkIfDifferenceNotExceedsThreshold(values.get(i), values.get(i + 1), 0.05)) {
+					isDifferenceWithinLimit = false;
+				}
+			}
+		} catch (NullPointerException ne) {
+			isDifferenceWithinLimit = false;
+		}
+		return isDifferenceWithinLimit;
+	}
 }
--- src/test/java/sensorval/SensorValidatorTest.java
+++ src/test/java/sensorval/SensorValidatorTest.java
@@ -2,6 +2,7 @@ package sensorval;
 
 import static org.junit.Assert.assertFalse;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,5 +33,28 @@ public class SensorValidatorTest {
 	public void reportsErrorWhenSOCNull() {
 		List<Double> socs = null;
 		assertFalse(SensorValidator.validateSOCreadings(socs));
+	}
+	@Test
+	public void reportsErrorWhenSOCIsEmpty() {
+		List<Double> socs = new ArrayList<>();
+		assertFalse(SensorValidator.validateSOCreadings(socs));
+	}
+	@Test
+	public void reportsErrorWhenCurrentIsEmpty() {
+		List<Double> currents = new ArrayList<>();
+		assertFalse(SensorValidator.validateCurrentreadings(currents));
+	}
+	@Test
+	public void reportsErrorWhenSOCReadingIsNull() {
+		Double[] readings = { 0.0, 0.01, null };
+		List<Double> socs = Arrays.asList(readings);
+		assertFalse(SensorValidator.validateSOCreadings(socs));
+	}
+
+	@Test
+	public void reportsErrorWhenCurrentReadingIsNull() {
+		Double[] readings = { 0.03, 0.03, 0.03, null };
+		List<Double> currents = Arrays.asList(readings);
+		assertFalse(SensorValidator.validateCurrentreadings(currents));
 	}
 }
